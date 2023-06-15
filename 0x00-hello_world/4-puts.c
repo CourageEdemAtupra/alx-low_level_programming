6704444e07b2901a0abfd640e84f56c88ead6102
Courage Edem Atupra <courageatu@yahoo.com>
2023-06-15 15:20:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,17 @@
 
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message using the puts function
+ *
+ * Return: 0 on successful execution
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
+puts("\"Programming is like building a multilingual puzzle\"");
 
-    return (0);
-
+return (0);
 }
