c2b542cbd08c3f5d408be163e17a8d0c8d35654d
Courage Edem Atupra <courageatu@yahoo.com>
2023-06-15 16:19:56 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 
 #include <stdio.h>
 
-int main(void) {
+/* Description: Prints a piece of art with proper grammar */
+
+int main(void)
+{
 /* Print a piece of art with proper grammar */
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return 0;
