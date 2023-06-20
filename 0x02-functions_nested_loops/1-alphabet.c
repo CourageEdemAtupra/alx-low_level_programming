0efba3466c082520df4492d447073069f53dd42b
Courage Edem Atupra <courageatu@yahoo.com>
2023-06-20 15:01:30 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
-/** * print_alphabet - lowercase alphabet followed by a new line
+/**
+ * print_alphabet - Prints the lowercase alphabet followed by a new line
  */
 void print_alphabet(void)
 {
 char alph = 'a';
+
 while (alph <= 'z')
 {
 _putchar(alph);
 alph++;
 }
+
 _putchar('\n');
 }
