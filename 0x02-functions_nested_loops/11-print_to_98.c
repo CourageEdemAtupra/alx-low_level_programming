5aa46b1ad6c4ce370c2bb73cb913535775d915e3
Courage Edem Atupra <courageatu@yahoo.com>
2023-06-21 01:06:28 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,60 +1,27 @@
 #include "main.h"
 
-void print_number(int n);
-
 /**
- * print_to_98 - Prints all natural numbers from n to 98
+ * print_to_98 - Prints all numbers from n to 98
  * @n: The starting number
  */
 void print_to_98(int n)
 {
-int i;
+while (1)
+{
+_putchar(n + '0');
 
-if (n <= 98)
-{
-for (i = n; i <= 98; i++)
-{
-if (i != 98)
-print_number(i);
-else
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
-if (i != 98)
-{
+if (n == 98)
+break;
+
 _putchar(',');
-_putchar(' ');
-}
-}
-}
+
+if (n < 98)
+n++;
 else
-{
-for (i = n; i >= 98; i--)
-{
-if (i != 98)
-print_number(i);
-else
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
-if (i != 98)
-{
-_putchar(',');
+n--;
+
 _putchar(' ');
-}
-}
 }
 
 _putchar('\n');
 }
-
-/**
- * print_number - Prints a number using _putchar
- * @n: The number to print
- */
-void print_number(int n)
-{
-if (n / 10 != 0)
-print_number(n / 10);
-
-_putchar(n % 10 + '0');
-}
-
