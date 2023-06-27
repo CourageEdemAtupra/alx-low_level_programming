1d1a49bd0cb5921984194cf0e1034aadef1e8f40
Courage Edem Atupra <courageatu@yahoo.com>
2023-06-27 06:33:26 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,18 +1,35 @@
 #include "main.h"
 #include <stdio.h>
+#include <limits.h>
 
 /**
- * main - check the code 
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
     int n;
 
     n = 402;
-    printf("n=%d\n", n);
+    printf("n = %d\n", n);
     reset_to_98(&n);
-    printf("n=%d\n", n);
+    printf("n = %d\n", n);
+
+    n = -4096;
+    printf("n = %d\n", n);
+    reset_to_98(&n);
+    printf("n = %d\n", n);
+
+    n = INT_MAX;
+    printf("n = %d\n", n);
+    reset_to_98(&n);
+    printf("n = %d\n", n);
+
+    n = INT_MIN;
+    printf("n = %d\n", n);
+    reset_to_98(&n);
+    printf("n = %d\n", n);
+
     return (0);
 }
